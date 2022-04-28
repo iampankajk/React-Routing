8f8042c18fa7275ff16cec2dda2a81586932ba6a
pankaj kumar <raipankaj150@gmail.com>
2022-04-28 21:26:40 +0530

    build

--- Write-Quotes/src/App.js
+++ Write-Quotes/src/App.js
@@ -1,30 +1,41 @@
+import React, { Suspense } from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import Layout from './components/layout/Layout';
+import LoadingSpinner from './components/UI/LoadingSpinner';
 import AllQuotes from './pages/AllQuotes';
-import NewQuote from './pages/NewQuote';
+// import NewQuote from './pages/NewQuote';
 import NotFound from './pages/NotFound';
 import QuoteDetail from './pages/QuoteDetail';
+
+const NewQuote = React.lazy(() => import('./pages/NewQuote'));
+
 
 function App() {
   return (
     <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <Redirect to='/quotes'></Redirect>
-        </Route>
-        <Route path='/quotes' exact>
-          <AllQuotes></AllQuotes>
-        </Route>
-        <Route path='/quotes/:quoteId'>
-          <QuoteDetail></QuoteDetail>
-        </Route>
-        <Route path='/new-quote'>
-          <NewQuote></NewQuote>
-        </Route>
-        <Route path='*'>
-          <NotFound></NotFound>
-        </Route>
-      </Switch>
+      <Suspense fallback={
+        <div className='centered'>
+          <LoadingSpinner />
+        </div>}>
+        <Switch>
+          <Route path='/' exact>
+            <Redirect to='/quotes'></Redirect>
+          </Route>
+          <Route path='/quotes' exact>
+            <AllQuotes></AllQuotes>
+          </Route>
+          <Route path='/quotes/:quoteId'>
+            <QuoteDetail></QuoteDetail>
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote></NewQuote>
+          </Route>
+          <Route path='*'>
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Suspense>
+
     </Layout>
 
   );
